865c6078c1fa07474ff6746f186b32428e50bc8d
JulioLopez <juljesus77@gmail.com>
2025-10-06 12:52:04 +0200

    Ejercicio_5

---
+++ Ejercicio_5.py
@@ -0,0 +1,5 @@
+
+horas=input("Introduce el número de horas que trabajas " )
+coste=input("Introduce el coste por hora " )
+operación= int(horas) * int(coste)
+print("El coste total es: " +  str(operación)) 
