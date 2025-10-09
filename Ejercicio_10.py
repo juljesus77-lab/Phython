848adaabad22634e1a6db68d6f42f51836d616b1
JulioLopez <juljesus77@gmail.com>
2025-10-09 10:46:12 +0200

    Ejercicio_10

---
+++ Ejercicio_10.py
@@ -0,0 +1,7 @@
+nºpayasos = input ("Introduzca el número de payasos: ") 
+nºmuñecas = input ("Introduzca el número de muñecas: ")
+peso_payaso = 112
+peso_muñeca = 75
+peso_total = (int(nºpayasos) * float(peso_payaso)) + int(nºmuñecas) * float (peso_muñeca)
+
+print ("El peso total del paquete es : " + str(peso_total))
