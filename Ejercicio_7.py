d13bf6a81135708b31897b5eb5a2c6c183a2c2d8
JulioLopez <juljesus77@gmail.com>
2025-10-09 09:59:01 +0200

    Ejercicio_7

---
+++ Ejercicio_7.py
@@ -0,0 +1,5 @@
+masa=float(input("Introduce tu masa en kg "))
+altura=float(input("Introduce tu altura en metros "))
+
+icm=masa/(altura ** 2) 
+print("Tu índice de masa corporal es  {:.2f}" .format(icm))
