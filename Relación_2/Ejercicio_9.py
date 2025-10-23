8f3d648167c9b46f009a2ca43d2bdd2bc52cb94b
JulioLopez <juljesus77@gmail.com>
2025-10-23 10:06:07 +0200

    Ejercicio_9.py(rel2)

---
+++ Relación_2/Ejercicio_9.py
@@ -0,0 +1,7 @@
+fecha = input ("Introduzca su fecha de nacimiento en el siguiente formato dd/mm/aaaa: ")
+
+dia = fecha.split ("/")[0]
+mes = fecha.split ("/")[1]
+año = fecha.split ("/")[2]
+print ("Día: " + dia +  " Mes: " + mes + " Año: " + año )
+
