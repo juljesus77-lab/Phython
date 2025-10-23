f98cb2c4a9c54e1124b893224ba384cd881af407
JulioLopez <juljesus77@gmail.com>
2025-10-23 09:57:16 +0200

    Ejercicio_8(rel2)

---
+++ Relación_2/Ejercicio_8.py
@@ -0,0 +1,10 @@
+
+numero =  input ("Introduzca el precio de un producto en euros: " ) 
+euros = numero.split (".")[0]
+centimos = numero.split (".")[1] , 2 
+
+print ("Tengo " + str(euros) + " euros" + " y " + str(centimos) + " céntimos" )
+
+
+
+
