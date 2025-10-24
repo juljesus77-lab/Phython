f3db275dcfca55cda59edd4514d2ddbb53440382
JulioLopez <juljesus77@gmail.com>
2025-10-24 14:17:50 +0200

    Ejercicio_3(rel3)

---
+++ Relación_3/Ejercicio_3.py
@@ -0,0 +1,10 @@
+num1 = int (input ("Introduzca un primer número ") )
+num2 = int (input ("Introduzca un segundo número "))
+operacion =  (num1 / num2)
+
+if (num2 == 0):
+    print ("Error")
+else:
+    print ("El resultado de la división es: "  + str(operacion) )
+
+#revisar
