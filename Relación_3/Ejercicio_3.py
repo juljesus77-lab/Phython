8dbd118a07acd40fae07b3970930b17f281ff80c
JulioLopez <juljesus77@gmail.com>
2025-10-24 14:27:18 +0200

    Ej_3(rel3-actualización)

--- Relación_3/Ejercicio_3.py
+++ Relación_3/Ejercicio_3.py
@@ -1,10 +1,10 @@
-num1 = int (input ("Introduzca un primer número ") )
-num2 = int (input ("Introduzca un segundo número "))
-operacion =  (num1 / num2)
+num1 = float (input ("Introduzca un primer número ") )
+num2 = float (input ("Introduzca un segundo número "))
 
 if (num2 == 0):
     print ("Error")
 else:
+    operacion =  float (num1 / num2)    #la operación dentro de la instrucción de donde se va a ejecutar
     print ("El resultado de la división es: "  + str(operacion) )
 
 #revisar
