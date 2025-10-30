a68bc652581c6f35a3f3a67cc7d74055b74dc1e0
JulioLopez <juljesus77@gmail.com>
2025-10-30 09:42:12 +0100

    Ejercicio_5(rel3)

--- Relación_3/Ejercicio_5.py
+++ Relación_3/Ejercicio_5.py
@@ -1,2 +1,8 @@
-E = int ( input ("Introduzca seu edad: ") ) 
+E = int ( input ("Introduzca su edad: ") ) 
 I = float ( input ("Introduzca sus ingresos mensuales: "))
+
+#¿el usuario tiene que tributar o no?
+if (E > 16 and I >= 1000):
+    print ("El usuario tiene que tributar")
+else:
+    print ("El usuario NO tiene que tributar")
