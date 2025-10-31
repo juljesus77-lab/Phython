5d3fcafdede709253703256460cf4c0940c3f0ee
JulioLopez <juljesus77@gmail.com>
2025-10-31 14:00:19 +0100

    Ejercicio_7(rel3)

--- Relación_3/Ejercicio_7.py
+++ Relación_3/Ejercicio_7.py
@@ -2,13 +2,12 @@ renta = int( input ("Introduzca su renta anual (€): ") )
 
 if (renta < 10000 ):
     print ("5%")
-elif (renta <= 10000 and renta <= 20000):
+elif (renta >= 10000 and renta <= 20000):
     print ("15%")
-elif (renta <= 20000 and renta <= 35000):
+elif (renta >= 20000 and renta <= 35000):
     print ("20%")
-elif (renta <= 35000 and renta <= 60000):
+elif (renta >= 35000 and renta <= 60000):
     print ("30%")
 else:
     print ("45%")
 
-    #revisar
